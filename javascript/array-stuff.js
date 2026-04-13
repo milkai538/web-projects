155e42c88eeddc4de5009df57e267d712116a7fc
milkai538 <milkai538@northwested.org>
2026-04-13 13:21:36 -0400

    sorting

---
+++ javascript/array-stuff.js
@@ -0,0 +1,41 @@
+// Aidan Milk
+// April 7th 2026
+// Array Methods Practice
+
+
+const heroes = ['Spider-man', 'Super Man', 'Chainsaw Man', 'Batman', 'Wonder Woman'];
+
+
+console.log(heroes.length);
+alert(`Initial Team Size: ${heroes.length}`);
+console.log(`Current Superhero Team: ${heroes}`);
+
+
+
+heroes.push('Unicorn Wo/Man');
+join ( );
+
+
+
+heroes.pop;
+const removedLastHero = ['Unicorn Wo/Man']
+console.log(`Removed Hero: ${removedLastHero}`);
+console.log(`New Hero Team: ${heroes}`);
+
+heroes.shift;
+removedLastHero.push(`Spider-man`);
+console.log(`Removed Hero: ${removedLastHero}`);
+console.log(`New Hero Team: ${heroes}`);
+
+
+heroes.unshift(`She-Ra`);
+console.log(`New Hero Team: ${heroes}`);
+
+
+console.log(heroes.indexOf('Batman'));
+const heroPosition = [`3`]; 
+
+
+
+console.log(heroes.length);
+
