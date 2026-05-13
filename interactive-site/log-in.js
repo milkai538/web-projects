6be4e2708f0e8b3963af9390dd2542bf003ead6d
milkai538 <milkai538@northwested.org>
2026-05-13 13:56:01 -0400

    just to be safe

--- interactive-site/log-in.js
+++ interactive-site/log-in.js
@@ -1,10 +1,12 @@
 
 
 const name = document.getElementById('name');
-
 const email = document.getElementById('email');
-
 const password = document.getElementById('password');
+
+const nameError = document.getElementById('nameError');
+const emailError = document.getElementById('emailError');
+const passwordError = document.getElementById('passwordError');
 
 const form = document.getElementById('form');
 
@@ -13,32 +15,26 @@ const form = document.getElementById('form');
 form.addEventListener('submit', (e)=> 
 {
 
-    if(name.value === '' || name.value == null)
+    var emailCheck = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+    if(name.value == '' || name.value == null)
 
     {
         e.preventDefault();
+        nameError.innerHTML = "Name is required.";
+    }
+
+        if(!email.value.match(emailCheck))
+    {
+        emailError.innerHTML = "Use a valid email.";
+    }
+
+    if(password.value.length <=5 )
+
+    {
+        e.preventDefault();
+        passwordError.innerHTML = "Password must be longer than 5 digits.";
     }
 
 })
 
-form.addEventListener('submit', (e)=> 
-{
-
-    if(email.value === '' || email.value == null)
-
-    {
-        e.preventDefault();
-    }
-
-})
-
-form.addEventListener('submit', (e)=> 
-{
-
-    if(password.value === '' || password.value == null)
-
-    {
-        e.preventDefault();
-    }
-
-})
