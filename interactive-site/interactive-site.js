e073cca41b2748b5fd1253af5496980c5993de39
milkai538 <milkai538@northwested.org>
2026-05-18 14:30:29 -0400

    disorganized and not working but we balllllll

--- interactive-site/interactive-site.js
+++ interactive-site/interactive-site.js
@@ -1,34 +1,50 @@
+const numbero = document.getElementById('numbero');
+const numberoerror = document.getElementById('numberoerror');
+const numberonumber = document.getElementById('numberonumber');
+
+const form = document.getElementById('form');
+
 form.addEventListener('submit', (e)=> 
 {
 
     if(numbero.value == '' || numbero.value == null)
     {e.preventDefault();
-    numberoError.innerHTML = "A number is required.";}  
+    numberoerror.innerHTML = "A number is required.";}  
         else
-    {numberoError.innerHTML = "";}
+    {numberoerror.innerHTML = "";}
+
+    if(numbero.value.length >= 2 )
+    {e.preventDefault();
+    numberoerror.innerHTML = "One, single digit number at a time.";}  
+        else
+    {numberoerror.innerHTML = "";}
 
 
-    if(numbero.value == '1')
-{numberonumber.innerHTML = "level 1 TG";}
+    if(numbero.value === '1')
+{e.preventDefault();
+    numberonumber.innerHTML = "level 1 TG";}
     else
 {numberonumber.innerHTML = "";}
 
 
 
-if(numbero.value == '2')
-{numberonumber.innerHTML = "level 2 TG";}
+if(numbero.value === '2')
+{e.preventDefault();
+    numberonumber.innerHTML = "level 2 TG";}
     else
 {numberonumber.innerHTML = "";}
 
 
-if(numbero.value == '3')
-{numberonumber.innerHTML = "level 3 TG";}
+if(numbero.value === '3')
+{e.preventDefault();
+    numberonumber.innerHTML = "level 3 TG";}
 else
 {numberonumber.innerHTML = "";}
 
 
-if(numbero.value == '4')
-{numberonumber.innerHTML = "level 4 TG";}
+if(numbero.value === '4')
+{e.preventDefault();
+    numberonumber.innerHTML = "level 4 TG";}
 else
 {numberonumber.innerHTML = "";}
 
