b3d368a1601b02bd438bf12624a723587bdb130c
milkai538 <milkai538@northwested.org>
2026-05-18 13:43:45 -0400

    save rq

--- interactive-site/interactive-site.js
+++ interactive-site/interactive-site.js
@@ -1,1 +1,15 @@
+const modelImages = ["coffeeshop.jpg" , "fourpixels.png"]
+
+const imgElement = document.getElementById('model-frame');
+
+const preload = new Image();
+preload.src = modelImages[1];
+
+imgElement.addEventListener('mouseenter', () => {
+     imgElement.src = modelImages[1];
+});
+
+imgElement.addEventListener('mouseleave', () => {
+     imgElement.src = modelImages[0];
+});
 
