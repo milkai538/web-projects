8f783b28d8fee2dc2a1ef8504860f06bdb2df37c
milkai538 <milkai538@northwested.org>
2026-05-19 14:37:22 -0400

    ssssssave all done

--- interactive-site/interactive-site.js
+++ interactive-site/interactive-site.js
@@ -28,22 +28,29 @@ form.addEventListener('submit', (e) => {
     switch (val) {
         case '1':
             e.preventDefault();
-            numberonumber.innerHTML = "level 1 TG";
+            numberonumber.innerHTML = "Phase 1 - Denial: Let's be honest, not many people will ever even begin to wonder if they can do something as powerful as manipulating reality itself. In this stage people tend to rationalize it, and will hopefully prevent further powers from this point on. Recommendation(s) for Encounter: Just don't bring it up, and you're good, don't worry.";
             break;
         case '2':
             e.preventDefault();
-            numberonumber.innerHTML = "level 2 TG";
+            numberonumber.innerHTML = "Phase 2 - Experimentation: This is often right after the person has discovered their ability (Or, in some cases, what they believe to be their ability) and they will begin testing to see the upper limits of what they can do. Yes, reality benders have limits. Usually the testing will either rise in a small number of jumps, or in a slow and steady method. Either way, this phase will last until the limit(s) at which point they move on to Phase 3. Recommendation(s) for Encounter: Try not making them too mad; don't want to be poofed away.";
             break;
         case '3':
             e.preventDefault();
-            numberonumber.innerHTML = "level 3 TG";
+            numberonumber.innerHTML = "Phase 3 - Stability: After feeling their limits out, a type green will hopefully stay here until death. This phase is often in tandem with the attempt to live a more normal life. Recommendation(s) for Encounter: Again, do not severely provoke, and the person will hopefully only need to be monitored. However.. It is with great sadness we tell you that most will progress unto phase 4.";
             break;
         case '4':
             e.preventDefault();
-            numberonumber.innerHTML = "level 4 TG";
+            numberonumber.innerHTML = "Phase 4 - The Child-God: 1%. Only 1% of reality benders in phase 4 will ever return to phase 3. Due to the primary missions of the SCP Foundation(To study anomalous entities.), we cannot condone the elimination of a reality bender for any purposes, and can affirm with you that it is entirely possible to save way more than 1/100 by reverting them to phase 3. Phase 4 is noteable for reality benders having megalomaniac desires, decreased empathy, and an inability to accept faults of the self. The only recommendation we can offer is to hide and contact either of our organizations for assistance.";
+            break;
+        case '5':
+            e.preventDefault();
+            numberonumber.innerHTML = "That is called a Type Black, or demi-deity. You only have an infitesimal chance to escape whatever situation you managed to get into.";
+            break;
+        case '0':
+            e.preventDefault();
+            numberonumber.innerHTML = "Rude to ignore such a simple instruction, but I digress. That is called a Thaumatologist, or a Type Blue (just think wizards and warlocks). These people are much less powerful than reality benders, but still hold the same capabilities, and are often hired by both of our organizations. Less powerful as in they need to do, as you'd expect, spells to make changes to reality, and face a usually negative feedback for doing so. Again, same capabilities, but they have vast expenses to pay first.";
             break;
         default:
-            // Optional: Handle numbers 0, 5, 6, 7, 8, 9 if submitted
             break;
     }
 });
